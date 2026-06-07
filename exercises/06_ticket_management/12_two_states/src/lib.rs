d34d03bccf16e8126e580348f190896401d48249
Laurent VERDOÏA <verdoialaurent@gmail.com>
2026-06-07 17:35:59 +0200

    06_ticket_management

--- exercises/06_ticket_management/12_two_states/src/lib.rs
+++ exercises/06_ticket_management/12_two_states/src/lib.rs
@@ -44,8 +44,21 @@ impl TicketStore {
         }
     }
 
-    pub fn add_ticket(&mut self, ticket: Ticket) {
-        self.tickets.push(ticket);
+    pub fn add_ticket(&mut self, draft: TicketDraft) -> TicketId {
+        let id = TicketId(self.tickets.len() as u64);
+
+        self.tickets.push(Ticket {
+            id: id,
+            title: draft.title,
+            description: draft.description,
+            status: Status::ToDo,
+        });
+
+        id
+    }
+
+    pub fn get(&self, id: TicketId) -> Option<&Ticket> {
+        self.tickets.get(id.0 as usize)
     }
 }
 
